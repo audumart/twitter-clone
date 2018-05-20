3267aa8fb5aab845db7cadda36c66fdae5783c29
System Administrator <root@Audus-MacBook-Pro.local>
2018-05-20 14:00:00 +0100

    user controller for logins

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -25,6 +25,26 @@ class UserController {
             })
         }
     }
+
+    async login ({request, auth, response}){
+        try {
+            // validate user credentials with JWT
+            const token = await auth.attempt(
+                request.input('email'),
+                request.input('password')
+            )
+
+            return response.json({
+                status: 'success',
+                data: token
+            })
+        } catch (error) {
+            response.status(400).json({
+                status: 'error',
+                message: 'Invalid email/password'
+            })
+        }
+    }
 }
 
 module.exports = UserController
