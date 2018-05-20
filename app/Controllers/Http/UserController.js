480ebe05c310f2e00d6f850bcd40e997b00fb912
System Administrator <root@Audus-MacBook-Pro.local>
2018-05-20 14:06:44 +0100

    adding me function

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -45,6 +45,29 @@ class UserController {
             })
         }
     }
+    async me ({auth, response}) {
+        const user = await User.query()
+        .where('id', auth.current.user.id)
+        .with('tweets', builder => {
+            builder.width('user')
+            builder.width('favorites')
+            builder.width('replies')
+        })
+        .with('following')
+        .with('followers')
+        .with('favorites')
+        .with('favorites.tweet', builder => {
+            builder.with('user')
+            builder.with('favorites')
+            builder.with('replies')
+        })
+        .firstOrFail()
+
+    return response.json({
+        status: 'success',
+        data: user
+    })
+    }
 }
 
 module.exports = UserController
