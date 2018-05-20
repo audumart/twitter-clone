6a8202aa6d56e9b0336f26fb493ab341bef04712
System Administrator <root@Audus-MacBook-Pro.local>
2018-05-20 13:52:11 +0100

    user controller for sign up

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,30 @@
+'use strict'
+const User = use('App/Models/User')
+
+class UserController {
+    async signup ({request, auth, response}){
+
+        const userData = request.only(['name', 'username', 'email','password'])
+
+        try{
+            // save user to db
+            const user = await User.create(userData)
+            // generate JWT token for user
+            const token = await auth.generate(user)
+
+            return response.json({
+
+                status: 'success',
+                data: token
+            })
+
+        } catch(error){
+            return response.status(400).json({
+                status: 'error',
+                message: 'There was a problem creating the user, try again later. '
+            })
+        }
+    }
+}
+
+module.exports = UserController
